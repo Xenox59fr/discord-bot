e29ea83f9f3f5eef25c2838cab50acacfca5da1c
Xenox59fr <69153808+Xenox59fr@users.noreply.github.com>
2025-05-06 00:44:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,18 @@ import json
 import os
 from dotenv import load_dotenv
 import datetime
+from flask import Flask
+import threading
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+threading.Thread(target=run).start()
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
