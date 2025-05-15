e3f2f3e89eadec40923355330fdc82fb9033c775
Xenox59fr <69153808+Xenox59fr@users.noreply.github.com>
2025-05-15 23:05:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -494,8 +494,8 @@ async def collection(ctx):
 
     cartes = cartes_joueurs[user_id]  # Liste des cartes de l'utilisateur
 
-    class CollectionView(ui.View):
-    def __init__(self, user_cartes):
+ class CollectionView(ui.View):
+     def __init__(self, user_cartes):
         super().__init__(timeout=None)
         self.user_cartes = user_cartes
 
