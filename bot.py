7afcc2bb6a9f7aedcd7c2fd92ad28270783ab867
Xenox59fr <69153808+Xenox59fr@users.noreply.github.com>
2025-05-19 19:23:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,9 +15,9 @@ from discord import ui
 from discord.ui import View, button  
 from discord import ButtonStyle   
 from collections import Counter
-
-
-
+from threading import Thread
+from keep_alive import keep_alive
+keep_alive()
 
 
 joueurs_cartes = {}
@@ -182,10 +182,14 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Bot actif."
 
 def run():
     app.run(host='0.0.0.0', port=8080)
+    
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
 threading.Thread(target=run).start()
 
